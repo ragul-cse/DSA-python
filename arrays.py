d2c9b1823fc4114d9e76ba26810ce1bd75a076a1
ragul-cse <ragulkamaradje@gmail.com>
2026-08-14 21:56:36 +0530

    largest number problem

--- arrays.py
+++ arrays.py
@@ -3,3 +3,14 @@ print("arrays:", numbers)
 print("first element:", numbers[0])
 print("last element:", numbers[-1])
 print("length:", len(numbers))
+
+
+numbers = [10, 25, 7, 42, 18, 31]
+
+largest = numbers[0]
+
+for number in numbers:
+    if number > largest:
+        largest = number
+
+print("Largest number:", largest)
